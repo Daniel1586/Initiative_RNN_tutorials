8b37f4e93fd89632f3bd8112afd7efc59e68f891
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-05-10 22:31:30 +0800

    增加reuters_mlp代码注释并调整格式

--- keras_working_with_text/reuters_mlp.py
+++ keras_working_with_text/reuters_mlp.py
@@ -15,31 +15,34 @@ max_words = 1000    # vocab大小
 batch_size = 32     # min-batch size
 epochs = 5          # 大循环次数
 
-print('Loading data...')
+# 数据集来源路透社新闻专线,共11228条新闻,标记46个类别
+# 每条数据被编码为一条索引序列(索引数字越小,代表单词出现次数越多)
+# num_words: 选取的每条数据里的索引值不能超过num_words
+# test_split: test data所占数据集比例
+print('========== 1.Loading data...')
 (x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words, test_split=0.2)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-
-# 分类数目
+print('----- train sequences', len(x_train))
+print('----- test  sequences', len(x_test))
 num_classes = np.max(y_train) + 1
-print(num_classes, 'classes')
+print('----- classes num', num_classes)
 
-print('Vectorizing sequence data...')
-tokenizer = Tokenizer(num_words=max_words)
-# 序列化,取df前1000大
+# 对每条词索引组成的数据(train/test)转换为0/1值序列,若单词出现则为1,否则为0
+print('========== 2.Vectorizing sequence data...')
+tokenizer = Tokenizer(num_words=max_words)  # 只记录max_words数量的单词信息
 x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
 x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+print('----- x_train shape:', x_train.shape)
+print('----- x_test  shape:', x_test.shape)
 
-print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
+# 对每条数据的类别标签(train/test)转换为0/1值序列
+print('Convert class vector to binary class matrix ''(for use with categorical_crossentropy)')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
+print('----- y_train shape:', y_train.shape)
+print('----- y_test  shape:', y_test.shape)
 
-print('Building model...')
+# 搭建网络模型
+print('========== 3.Building model...')
 model = Sequential()
 # 第一层
 model.add(Dense(512, input_shape=(max_words,)))
