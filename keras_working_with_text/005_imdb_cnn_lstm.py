4cbf1a0b76f01fa2b2b14a6c2779e07130b0255b
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-05-18 23:30:54 +0800

    修正005_imdb_cnn_lstm代码注释并调整格式

--- keras_working_with_text/005_imdb_cnn_lstm.py
+++ keras_working_with_text/005_imdb_cnn_lstm.py
@@ -13,64 +13,54 @@ from keras.layers import Conv1D, MaxPooling1D
 from keras.datasets import imdb
 
 # Embedding
-max_features = 20000
-maxlen = 100
-embedding_size = 128
+max_features = 20000        # vocab大小
+maxlen = 100                # 每条样本数据长度
+embedding_size = 128        # 词向量维度
 
 # Convolution
-kernel_size = 5
-filters = 64
+kernel_size = 5             # 整数或由单个整数构成的list/tuple,卷积核的空域或时域窗长度
+filters = 64                # 1D-CNN卷积核的数目(即输出的维度)
 pool_size = 4
 
 # LSTM
 lstm_output_size = 70
 
 # Training
-batch_size = 30
-epochs = 5
+batch_size = 30             # min-batch size
+epochs = 5                  # 循环次数
 
-'''
-Note:
-batch_size is highly sensitive.
-Only 2 epochs are needed as the dataset is very small.
-'''
-
-print('Loading data...')
+# 数据集来源IMDB影评,共50000条影评,标记正面/负面两种评价
+# 每条数据被编码为一条索引序列(索引数字越小,代表单词出现次数越多)
+# num_words: 选取的每条数据里的索引值不能超过num_words
+print('========== 1.Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+print('----- train sequences', len(x_train))
+print('----- test  sequences', len(x_test))
 
-print('Pad sequences (samples x time)')
+# 对每条词索引组成的数据进行长度对齐,去掉数据前面或后面多余的单词;长度不够插入0
+print('========== 2.Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+print('----- x_train shape:', x_train.shape)
+print('----- x_test shape:', x_test.shape)
 
-print('Build model...')
-
+# 搭建神经网络模型
+print('========== 3.Build model...')
 model = Sequential()
-model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+
+# input_dim=max_features单词表大小,output_dim=embedding_dims=128为词向量维度
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))     # 输出(*,100,50)
 model.add(Dropout(0.25))
-model.add(Conv1D(filters,
-                 kernel_size,
-                 padding='valid',
-                 activation='relu',
-                 strides=1))
+model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
 model.add(MaxPooling1D(pool_size=pool_size))
 model.add(LSTM(lstm_output_size))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-print('Train...')
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test))
+# 神经网络编译/训练/测试集测试性能
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
 score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
-print('Test score:', score)
-print('Test accuracy:', acc)
+print('----- Test score:', score)
+print('----- Test accuracy:', acc)
 
